6dfd22c9eed158e3e398c0cbd7b1a6bfb68ae5e6
Kartik <kartikkc397@outlook.com>
2023-08-04 23:45:38 +0530

    Completed the blog website

---
+++ app.js
@@ -0,0 +1,83 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const lodash = require("lodash");
+const app = express();
+
+const homeContent = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas aut magnam consequuntur. Numquam labore accusamus soluta expedita tempora non nisi in, hic nam dolore culpa quas autem enim! Beatae, ipsa.";
+const aboutContent = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas aut magnam consequuntur. Numquam labore accusamus soluta expedita tempora non nisi in, hic nam dolore culpa quas autem enim! Beatae, ipsa.";
+const contactContent = "If you like my Blogs, Feel free to drop a message.";
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+app.set("views", __dirname + "/views");
+let date = new Date();
+const options = {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+}
+let day = date.toLocaleDateString("en-US", options)
+let posts = [{
+    title: "New Day",
+    body: "This is my blog site and I have been working on it since last 6 days!"
+}];
+let messages =[];
+app.get("/", (req, res) => {
+    res.render("home", { content: homeContent, dateAndDay: day, postContent: posts });
+})
+
+app.get("/about", (req, res) => {
+    res.render("about", { about: aboutContent, dateAndDay: day });
+})
+
+app.get("/contact", (req, res) => {
+    res.render("contact", { contact: contactContent });
+})
+
+app.get("/compose", (req, res) => {
+    res.render("compose");
+})
+
+app.post("/compose", (req, res) => {
+    const post = {
+        title: req.body.title,
+        body: req.body.content
+    }
+    posts.push(post);
+    console.log(posts);
+    res.redirect("/");
+})
+
+app.post("/contact", (req, res) => {
+    const message = {
+        name : req.body.name,
+        email: req.body.email,
+        message : req.body.message
+    }
+    messages.push(message);
+    console.log(messages);
+    res.redirect("sucess");
+})
+app.get("/post", (req, res) => {
+    res.render("post", { postContent: posts, dateAndDay: day });
+})
+
+app.get("/post/:title", (req, res) => {
+    const postTitle = req.params.title;
+    const lowerCasePostTitle = lodash.lowerCase(postTitle);
+    posts.forEach((post) => {
+        const storedTitle = lodash.lowerCase(post.title);
+
+        if (storedTitle === lowerCasePostTitle) {
+            console.log("[Search] Success-200 : Match Found");
+            res.render("post", { postTitle: post.title, postBody: post.body, dateAndDay: day });
+        }
+    })
+})
+
+app.listen(process.env.PORT ||3000, () => {
+    console.log("[Status] The server is running on port: 3000");
+})
